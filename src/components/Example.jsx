65ca952a1ffaee07ed600c94cc3d273eb44a6b03
RohanWork <rohankatarework@gmail.com>
2023-10-24 00:08:49 +0530

    Revert "done ui changes according to previous build"
    This reverts commit f7b052edc402af8f0e5b8fd698511f62b0ad33f0.

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
@@ -16,16 +15,14 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Navbar2() {
+export default function Example() {
   return (
-    <Disclosure as="nav" className="Nav-color w-full fixed top-0 z-50 h-16 shadow-lg">
+    <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
-              
-              
-              <div className="absolute inset-y-0 left-0 z-50 flex items-center sm:hidden">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="absolute -inset-0.5" />
@@ -37,11 +34,7 @@ export default function Navbar2() {
                   )}
                 </Disclosure.Button>
               </div>
-              
-              
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-               
-               {/* tailwind logo */}
                 <div className="flex flex-shrink-0 items-center">
                   <img
                     className="h-8 w-auto"
@@ -49,7 +42,6 @@ export default function Navbar2() {
                     alt="Your Company"
                   />
                 </div>
-                
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
@@ -57,7 +49,7 @@ export default function Navbar2() {
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          item.current ? 'bg-gray-900 text-white z-50' : 'text-gray-300 hover:bg-gray-700 hover:text-white z-50',
+                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                           'rounded-md px-3 py-2 text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
@@ -68,17 +60,15 @@ export default function Navbar2() {
                   </div>
                 </div>
               </div>
-
-              {/* profile and bell icon in navbar */}
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                {/* <button
+                <button
                   type="button"
                   className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                 >
                   <span className="absolute -inset-1.5" />
                   <span className="sr-only">View notifications</span>
                   <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button> */}
+                </button>
 
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
@@ -107,7 +97,7 @@ export default function Navbar2() {
                         {({ active }) => (
                           <a
                             href="#"
-                            className={classNames(active ? 'bg-gray-400' : '', 'block px-4 py-2 text-sm text-black')}
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             Your Profile
                           </a>
@@ -117,7 +107,7 @@ export default function Navbar2() {
                         {({ active }) => (
                           <a
                             href="#"
-                            className={classNames(active ? 'bg-gray-400' : '', 'block px-4 py-2 text-sm text-black')}
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             Settings
                           </a>
@@ -127,7 +117,7 @@ export default function Navbar2() {
                         {({ active }) => (
                           <a
                             href="#"
-                            className={classNames(active ? 'bg-gray-400' : '', 'block px-4 py-2 text-sm text-black')}
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             Sign out
                           </a>
@@ -140,9 +130,8 @@ export default function Navbar2() {
             </div>
           </div>
 
-          {/* panel option menus when screen size is minimum */}
           <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2 Nav-color">
+            <div className="space-y-1 px-2 pb-3 pt-2">
               {navigation.map((item) => (
                 <Disclosure.Button
                   key={item.name}
